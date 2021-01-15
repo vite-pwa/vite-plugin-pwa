131e493fc7c7246d20545977cc84877d1db10c89
hanlee <me@hanlee.co>
2021-01-16 03:30:55 +0900

    feat: add injectManifest strategies

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -3,12 +3,14 @@ import Vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 
 const config: UserConfig = {
-  build: {
-    base: 'test'
-  },
+  // build: {
+  //   base: 'test',
+  // },
   plugins: [
     Vue(),
-    VitePWA(),
+    VitePWA({
+      strategies: 'injectManifest',
+    }),
   ],
 }
 
