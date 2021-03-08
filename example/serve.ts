fc31eecb937f8c0f211d172f84bbde156e036c6b
Anthony Fu <anthonyfu117@hotmail.com>
2021-03-08 17:34:12 +0800

    chore: cleanup

--- example/serve.ts
+++ example/serve.ts
@@ -1,23 +1,15 @@
-/* eslint-disable */
-'use strict'
+import { createServer } from 'http'
+import { createServer as createHttpsServer } from 'https'
+import { readFileSync } from 'fs'
+import { Server } from 'node-static'
 
-import { createServer } from 'http';
-import { createServer as _createServer } from 'https';
-import { readFileSync } from "fs";
-import { Server } from 'node-static';
-
-const fileServer  = new Server('./dist')
+const fileServer = new Server('./dist')
 
 const file = readFileSync('.env.local.json', 'utf8')
 
 const { ca, key, cer, hostname = 'localhost', httpsPort = 443, httpPort = 80 } = JSON.parse(file)
 
-const httpsRedirect = async(req, res) => {
-  res.writeHead(301, { "Location": `https://${hostname}:${httpsPort}${req.url}` });
-  res.end()
-}
-
-const secureServer = _createServer({
+const secureServer = createHttpsServer({
   ca: readFileSync(ca),
   key: readFileSync(key),
   cert: readFileSync(cer),
@@ -29,7 +21,10 @@ secureServer.listen(httpsPort, () => {
   console.log(`Server running on https://${hostname}:${httpsPort}/`)
 })
 
-const redirectServer = createServer(httpsRedirect)
+const redirectServer = createServer((req, res) => {
+  res.writeHead(301, { Location: `https://${hostname}:${httpsPort}${req.url}` })
+  res.end()
+})
 
 redirectServer.listen(httpPort, () => {
   console.log(`Redirect server running on http://${hostname}:${httpPort}/`)
