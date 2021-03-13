ee6af25085b5a92d5aa9991838aab244d108d9ae
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2021-03-13 05:20:45 +0100

    feat: add transition and loading state

--- examples/vue-router/src/App.vue
+++ examples/vue-router/src/App.vue
@@ -1,22 +1,41 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import { useTimeAgo } from '@vueuse/core'
 // import ReloadPrompt from './ReloadPrompt.vue'
 
 // replaced dyanmicaly
 const date = '__DATE__'
 const timeAgo = useTimeAgo(date)
+const loaing = ref(false)
 </script>
 
 <template>
   <div>Built at: {{ date }} ({{ timeAgo }})</div>
   <br>
-  <router-view v-slot="{ Component }">
-    <suspense>
-      <template #fallback>
-        <div>Loading...</div>
+  <!--  <router-view />-->
+  <div :class="{ loading }">
+    <router-view v-slot="{ Component }">
+      <template v-if="Component">
+        <transition mode="out-in">
+          <keep-alive>
+            <suspense
+              @pending="loading = true"
+              @resolve="loading = false"
+            >
+              <template #default>
+                <!-- this will not work: our components has more than one root -->
+                <div>
+                  <component :is="Component" />
+                </div>
+              </template>
+              <template #fallback>
+                <div>Loading...</div>
+              </template>
+            </suspense>
+          </keep-alive>
+        </transition>
       </template>
-      <component :is="Component" />
-    </suspense>
-  </router-view>
+    </router-view>
+  </div>
 <!--  <ReloadPrompt />-->
 </template>
