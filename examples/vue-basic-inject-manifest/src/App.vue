d24dc80e2da472ea565a43414deb8b329680e924
userquin <joaquin.sanchez@fi2net.es>
2021-06-13 07:04:57 +0200

    test: add web worker to test vite web worker plugin (#76)

--- examples/vue-basic-inject-manifest/src/App.vue
+++ examples/vue-basic-inject-manifest/src/App.vue
@@ -1,15 +1,51 @@
 <script setup lang="ts">
+import { ref, onBeforeMount } from 'vue'
 import { useTimeAgo } from '@vueuse/core'
+import Worker from './my-worker?worker'
+
 import ReloadPrompt from './ReloadPrompt.vue'
+
+const pong = ref(null)
+const mode = ref(null)
+const worker = new Worker()
 
 // replaced dyanmicaly
 const date = '__DATE__'
 const timeAgo = useTimeAgo(date)
+
+const runWorker = async() => {
+  worker.postMessage('ping')
+}
+const resetMessage = async() => {
+  worker.postMessage('clear')
+}
+const messageFromWorker = async({ data: { msg, mode: useMode } }) => {
+  pong.value = msg
+  mode.value = useMode
+}
+
+onBeforeMount(() => {
+  worker.addEventListener('message', messageFromWorker)
+})
 </script>
 
 <template>
   <img src="/favicon.svg" width="60" height="60">
   <br>
   <div>Built at: {{ date }} ({{ timeAgo }})</div>
+  <br />
+  <br />
+  <button @click="runWorker">
+    Ping web worker
+  </button>
+  &#160;&#160;
+  <button @click="resetMessage">
+    Reset message
+  </button>
+  <br />
+  <br />
+  <template v-if="pong">
+    Response from web worker: <span> Message: {{ pong }} </span>&#160;&#160;<span> Using ENV mode: {{ mode }}</span>
+  </template>
   <ReloadPrompt />
 </template>
---
+++ examples/vue-basic-inject-manifest/src/my-worker.js
@@ -0,0 +1,13 @@
+import { msg, mode } from './workerImport'
+
+let counter = 1
+
+self.onmessage = (e) => {
+  if (e.data === 'ping') {
+    self.postMessage({ msg: `${msg} - ${counter++}`, mode })
+  }
+  else if (e.data === 'clear') {
+    counter = 1
+    self.postMessage({ msg: null, mode: null })
+  }
+}
