8f1dd26315d2afad58e7bed89d7771c14465da8c
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2021-08-04 15:26:58 +0200

    fix: use only 1 img and switch the source

--- docs/.vitepress/theme/components/PwaLogo.vue
+++ docs/.vitepress/theme/components/PwaLogo.vue
@@ -1,8 +1,11 @@
-<template>
-  <img v-show="!isDark" src="/icon_light.svg" height="50" class="align-middle" alt="Vite Plugin PWA Logo"/>
-  <img v-show="isDark" src="/icon_dark.svg" height="50" class="align-middle" alt="Vite Plugin PWA Logo"/>
-</template>
-
 <script setup lang="ts">
 import { isDark } from '../composables/dark'
+
+const icon = computed(() => {
+  return isDark ? '/icon_dark.svg' : '/icon_light.svg'
+})
 </script>
+
+<template>
+  <img :src="icon" width="50" height="50" class="align-middle" alt="Vite Plugin PWA Logo"/>
+</template>
