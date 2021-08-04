45d69134ba9402845c2be31e2783504ff21a318f
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2021-08-04 15:54:26 +0200

    fix: add height to logo image

--- docs/.vitepress/theme/components/PwaLogo.vue
+++ docs/.vitepress/theme/components/PwaLogo.vue
@@ -1,11 +1,8 @@
 <script setup lang="ts">
 import { isDark } from '../composables/dark'
-
-const icon = computed(() => {
-  return isDark ? '/icon_dark.svg' : '/icon_light.svg'
-})
 </script>
 
 <template>
-  <img :src="icon" width="50" height="50" class="align-middle" alt="Vite Plugin PWA Logo"/>
+  <img v-show="!isDark" src="/icon_light.svg" width="50" height="50" class="align-middle" alt="Vite Plugin PWA Logo"/>
+  <img v-show="isDark" src="/icon_dark.svg" width="50" height="50" class="align-middle" alt="Vite Plugin PWA Logo"/>
 </template>
