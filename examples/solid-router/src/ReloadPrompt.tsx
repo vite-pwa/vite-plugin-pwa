cb2a4546713498b6363ad9440669663d60e75018
David D <1168397+davedbase@users.noreply.github.com>
2021-11-17 11:16:45 -0500

    Updated solid-router demo with Show component

--- examples/solid-router/src/ReloadPrompt.tsx
+++ examples/solid-router/src/ReloadPrompt.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/react-in-jsx-scope,react/no-unknown-property */
-import { Component } from 'solid-js'
+import { Component, Show } from 'solid-js'
 import { useRegisterSW } from 'virtual:pwa-register/solid'
 import styles from './ReloadPrompt.module.css'
 
@@ -38,18 +38,22 @@ const ReloadPrompt: Component = () => {
 
   return (
     <div class={styles.Container}>
-      { (offlineReady() || needRefresh())
-          && <div class={styles.Toast}>
-            <div class={styles.Message}>
-              { offlineReady()
-                ? <span>App ready to work offline</span>
-                : <span>New content available, click on reload button to update.</span>
-              }
-            </div>
-            { needRefresh() && <button class={styles.ToastButton} onClick={() => updateServiceWorker(true)}>Reload</button> }
-            <button class={styles.ToastButton} onClick={() => close()}>Close</button>
-          </div>
-      }
+      <Show when={offlineReady() || needRefresh()}>
+        <div class={styles.Toast}>
+         <div class={styles.Message}>
+           <Show
+             fallback={<span>New content available, click on reload button to update.</span>}
+             when={offlineReady()}
+           >
+             <span>App ready to work offline</span> 
+           </Show>
+         </div>
+         <Show when={needRefresh()}>
+           <button class={styles.ToastButton} onClick={() => updateServiceWorker(true)}>Reload</button>
+         </Show>
+         <button class={styles.ToastButton} onClick={() => close()}>Close</button>
+        </div>
+      </Show>
     </div>
   )
 }
