26e4dd96f962fd7bb02b6685d21f46e02326ca19
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2022-06-07 17:14:51 +0200

    chore: add webmanifest to denylist on custom sw + fix sveltekit latest changes

--- examples/sveltekit-pwa/src/claims-sw.ts
+++ examples/sveltekit-pwa/src/claims-sw.ts
@@ -10,8 +10,16 @@ precacheAndRoute(self.__WB_MANIFEST)
 // clean old assets
 cleanupOutdatedCaches()
 
+
+let denylist: undefined | RegExp[]
+if (import.meta.env.DEV)
+  denylist = [/^\/_app\/manifest.webmanifest$/]
+
 // to allow work offline
-registerRoute(new NavigationRoute(createHandlerBoundToURL('/')))
+registerRoute(new NavigationRoute(
+  createHandlerBoundToURL('/'),
+  { denylist },
+))
 
 self.skipWaiting()
 clientsClaim()
