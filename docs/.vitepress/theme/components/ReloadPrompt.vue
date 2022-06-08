fd4a1f615c372b53ff26914ed734330181891773
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2022-06-09 01:30:07 +0200

    chore: use teloport on pwa-prompt

--- docs/.vitepress/theme/components/ReloadPrompt.vue
+++ docs/.vitepress/theme/components/ReloadPrompt.vue
@@ -22,28 +22,30 @@ onBeforeMount(async () => {
 </script>
 
 <template>
-  <div
-    v-if="needRefresh"
-    class="pwa-toast z-100 bg-$vp-c-bg border border-$pwa-divider fixed right-0 bottom-0 m-6 px-6 py-4 rounded shadow-xl"
-    role="alertdialog"
-    aria-labelledby="pwa-message"
-  >
-    <div id="pwa-message" class="mb-3">
-      New content available, click the reload button to update.
+  <Teleport to="body">
+    <div
+      v-if="needRefresh"
+      class="pwa-toast z-100 bg-$vp-c-bg border border-$pwa-divider fixed right-0 bottom-0 m-6 px-6 py-4 rounded shadow-xl"
+      role="alertdialog"
+      aria-labelledby="pwa-message"
+    >
+      <div id="pwa-message" class="mb-3">
+        New content available, click the reload button to update.
+      </div>
+      <button
+        type="button"
+        class="border border-$pwa-divider bg-$vp-c-brand text-black mr-2 px-3 py-1 rounded hover:bg-$c-brand-light"
+        @click="updateServiceWorker?.()"
+      >
+        Reload
+      </button>
+      <button
+        type="button"
+        class="border border-$pwa-divider text-black mr-2 px-3 py-1 rounded hover:bg-$c-brand-light"
+        @click="close"
+      >
+        Close
+      </button>
     </div>
-    <button
-      type="button"
-      class="border border-$pwa-divider bg-$vp-c-brand text-black mr-2 px-3 py-1 rounded hover:bg-$c-brand-light"
-      @click="updateServiceWorker?.()"
-    >
-      Reload
-    </button>
-    <button
-      type="button"
-      class="border border-$pwa-divider text-black mr-2 px-3 py-1 rounded hover:bg-$c-brand-light"
-      @click="close"
-    >
-      Close
-    </button>
-  </div>
+  </Teleport>
 </template>
