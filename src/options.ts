f809876f5a7428bd5fba11e5654774361d182ab8
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2022-07-13 20:03:58 +0200

    chore: move SvelteKit `dontCacheBustURLsMatching` configuration to `options` module

--- src/options.ts
+++ src/options.ts
@@ -31,6 +31,22 @@ function resolveSwPaths(injectManifest: boolean, root: string, srcDir: string, o
   }
 }
 
+// Vite generates <name>.<hash>.<ext> layout while SvelteKit generates <name>-<hash>.<ext>
+// Vite and SvelteKit are not aligned: pwa plugin will use /\.[a-f0-9]{8}\./ by default: #164 optimize workbox work
+// All assets will go to the immutable folder, and so, there is no need to calculate its revision for the sw's precache manifest
+export function configureSvelteKitOptions(options: Partial<VitePWAOptions>) {
+  if (options.strategies === 'injectManifest') {
+    options.injectManifest = options.injectManifest ?? {}
+    if (!options.injectManifest.dontCacheBustURLsMatching)
+      options.injectManifest.dontCacheBustURLsMatching = /-[a-f0-9]{8}\./
+  }
+  else {
+    options.workbox = options.workbox ?? {}
+    if (!options.workbox.dontCacheBustURLsMatching)
+      options.workbox.dontCacheBustURLsMatching = /-[a-f0-9]{8}\./
+  }
+}
+
 export async function resolveOptions(options: Partial<VitePWAOptions>, viteConfig: ResolvedConfig): Promise<ResolvedVitePWAOptions> {
   const root = viteConfig.root
   const pkg = fs.existsSync('package.json')
--- src/plugins/main.ts
+++ src/plugins/main.ts
@@ -1,7 +1,7 @@
 import type { Plugin } from 'vite'
 import { VIRTUAL_MODULES, VIRTUAL_MODULES_MAP, VIRTUAL_MODULES_RESOLVE_PREFIX } from '../constants'
 import { generateRegisterSW } from '../modules'
-import { resolveOptions } from '../options'
+import { configureSvelteKitOptions, resolveOptions } from '../options'
 import { createAPI } from '../api'
 import type { PWAPluginContext } from '../context'
 import { swDevOptions } from './dev'
@@ -16,24 +16,11 @@ export function MainPlugin(ctx: PWAPluginContext): Plugin {
       // add support for new SvelteKit Vite Plugin.
       // SvelteKit Plugin will not be included in SSR build,
       // we need this global to detect the plugin on client build.
-      if (!ctx.userOptions.disable && !config.build.ssr) {
+      if (!ctx.userOptions.disable && !config.build.ssr)
         ctx.lookupSvelteKitPlugin()
-      }
-      else if (!ctx.userOptions.disable && config.build.ssr && ctx.isSvelteKitPluginPresent()) {
-        // Vite generates <name>.<hash>.<ext> layout while SvelteKit generates <name>-<hash>.<ext>
-        // Vite and SvelteKit are not aligned: pwa plugin will use /\.[a-f0-9]{8}\./ by default: #164 optimize workbox work
-        // All assets will go to the immutable folder, and so, there is no need to calculate its revision for the sw's precache manifest
-        if (ctx.userOptions.strategies === 'injectManifest') {
-          ctx.userOptions.injectManifest = ctx.userOptions.injectManifest ?? {}
-          if (!ctx.userOptions.injectManifest.dontCacheBustURLsMatching)
-            ctx.userOptions.injectManifest.dontCacheBustURLsMatching = /-[a-f0-9]{8}\./
-        }
-        else {
-          ctx.userOptions.workbox = ctx.userOptions.workbox ?? {}
-          if (!ctx.userOptions.workbox.dontCacheBustURLsMatching)
-            ctx.userOptions.workbox.dontCacheBustURLsMatching = /-[a-f0-9]{8}\./
-        }
-      }
+      else if (!ctx.userOptions.disable && config.build.ssr && ctx.isSvelteKitPluginPresent())
+        configureSvelteKitOptions(ctx.userOptions)
+
       ctx.options = await resolveOptions(ctx.userOptions, config)
     },
     resolveId(id) {
