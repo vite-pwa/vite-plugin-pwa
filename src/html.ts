cf7b7ec964609c37bdeb6f9fc172e64b6a1dbc64
ARYX <arielcustrin@gmail.com>
2022-08-21 20:30:19 +0200

    lint

--- src/html.ts
+++ src/html.ts
@@ -3,11 +3,11 @@ import type { ResolvedVitePWAOptions } from './types'
 
 export function generateSimpleSWRegister(options: ResolvedVitePWAOptions, dev: boolean) {
   const path = dev ? `${options.base}${DEV_SW_NAME}` : `${options.base}${options.filename}`
-  
+
   // we are using HMR to load this script: DO NOT ADD window::load event listener
   if (dev) {
-	const swType = options.devOptions.type ? options.devOptions.type : "classic";
-    return `if('serviceWorker' in navigator) navigator.serviceWorker.register('${path}', { scope: '${options.scope}', type: swType })`
+    const swType = options.devOptions.type ? options.devOptions.type : 'classic'
+    return `if('serviceWorker' in navigator) navigator.serviceWorker.register('${path}', { scope: '${options.scope}', type: ${swType} })`
   }
   return `
 if('serviceWorker' in navigator) {
