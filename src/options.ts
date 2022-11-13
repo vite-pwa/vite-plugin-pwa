44da9f064fd9d19a29a9e624f9438693b88b999b
userquin <userquin@gmail.com>
2022-11-13 17:06:48 +0100

    chore: wrong regex for injectManifest

--- src/options.ts
+++ src/options.ts
@@ -85,7 +85,7 @@ export async function resolveOptions(options: Partial<VitePWAOptions>, viteConfi
     swSrc,
     swDest,
     globDirectory: outDirRoot,
-    dontCacheBustURLsMatching: /[.-].[a-f0-9]{8}\./,
+    dontCacheBustURLsMatching: /[.-][a-f0-9]{8}\./,
     injectionPoint: 'self.__WB_MANIFEST',
   }
 
