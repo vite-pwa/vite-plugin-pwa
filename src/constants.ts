f03a3b51a4f7cebb00c14b4d088513c8e580cfa2
userquin <userquin@gmail.com>
2023-05-24 21:17:08 +0200

    fix: custom sw build failing

--- src/constants.ts
+++ src/constants.ts
@@ -31,4 +31,3 @@ export const DEV_SW_VIRTUAL = `${VIRTUAL_MODULES_RESOLVE_PREFIX}pwa-entry-point-
 export const RESOLVED_DEV_SW_VIRTUAL = `\0${DEV_SW_VIRTUAL}`
 export const DEV_READY_NAME = 'vite-pwa-plugin:dev-ready'
 export const DEV_REGISTER_SW_NAME = 'vite-plugin-pwa:register-sw'
-
