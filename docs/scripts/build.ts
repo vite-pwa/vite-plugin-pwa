7c3439e30b118e548db69d8366d5b2bc01c69b1c
userquin <userquin@gmail.com>
2023-08-29 15:56:40 +0200

    chore: remove ts ignore from docs build scripts

--- docs/scripts/build.ts
+++ docs/scripts/build.ts
@@ -5,10 +5,9 @@ import { pwa } from './pwa'
 
 export async function buildEnd() {
   await optimizePages()
-  // @ts-expect-error Vite 3 and plugin types, just ignore it
   const config = await resolveConfig({ plugins: [pwa()] }, 'build', 'production')
   // when `vite-plugin-pwa` is presented, use it to regenerate SW after rendering
-  const pwaPlugin: VitePluginPWAAPI = config.plugins.find(i => i.name === 'vite-plugin-pwa')?.api
+  const pwaPlugin: VitePluginPWAAPI | undefined = config.plugins.find(i => i.name === 'vite-plugin-pwa')?.api
   if (pwaPlugin && pwaPlugin.generateSW && !pwaPlugin.disabled)
     await pwaPlugin.generateSW()
 }
