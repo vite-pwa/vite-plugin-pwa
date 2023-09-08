d5a20bef9e1212754d7f86fd46092cccb30f8843
Joaquín Sánchez <userquin@gmail.com>
2023-09-08 23:47:38 +0200

    chore: update examples including tsconfig and type module (#564)

--- examples/vue-router/src/vite-env.d.ts
+++ examples/vue-router/src/vite-env.d.ts
@@ -1,3 +1,3 @@
 /// <reference types="vite/client" />
-/// <reference types="vite-plugin-pwa/preact" />
 /// <reference types="vite-plugin-pwa/info" />
+/// <reference types="vite-plugin-pwa/vue" />
