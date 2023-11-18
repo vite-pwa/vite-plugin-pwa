0a725838ee223854bbbbefcfa0e2e44338e05ea3
userquin <userquin@gmail.com>
2023-11-18 17:50:53 +0100

    chore: normalize assetsDir

--- src/options.ts
+++ src/options.ts
@@ -5,7 +5,7 @@ import type { ResolvedConfig } from 'vite'
 import type { GenerateSWOptions, InjectManifestOptions } from 'workbox-build'
 import type { ManifestOptions, ResolvedVitePWAOptions, VitePWAOptions } from './types'
 import { configureStaticAssets } from './assets'
-import { resolveBasePath } from './utils'
+import { resolveBasePath, slash} from './utils'
 import { defaultInjectManifestVitePlugins } from './constants'
 
 function resolveSwPaths(injectManifest: boolean, root: string, srcDir: string, outDir: string, filename: string): {
@@ -72,7 +72,7 @@ export async function resolveOptions(options: Partial<VitePWAOptions>, viteConfi
   const outDirRoot = resolve(root, outDir)
   const scope = options.scope || basePath
 
-  let assetsDir = viteConfig.build.assetsDir
+  let assetsDir = slash(viteConfig.build.assetsDir ?? 'assets')
   if (assetsDir[assetsDir.length - 1] !== '/')
     assetsDir += '/'
 
