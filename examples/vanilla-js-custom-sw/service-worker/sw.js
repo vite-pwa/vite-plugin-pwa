59644ec4b8d890e5c4714f7b390253b06e6d3d52
userquin <userquin@gmail.com>
2024-02-15 19:14:56 +0100

    fix: use `swDest` folder to resolve mjs and map files in Vite build

---
+++ examples/vanilla-js-custom-sw/service-worker/sw.js
@@ -0,0 +1,24 @@
+/// <reference lib="webworker" />
+import { cleanupOutdatedCaches, createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching'
+import { clientsClaim } from 'workbox-core'
+import { NavigationRoute, registerRoute } from 'workbox-routing'
+
+// self.__WB_MANIFEST is default injection point
+precacheAndRoute(self.__WB_MANIFEST)
+
+// clean old assets
+cleanupOutdatedCaches()
+
+/** @type {RegExp[] | undefined} */
+let allowlist
+if (import.meta.env.DEV)
+  allowlist = [/^\/$/]
+
+// to allow work offline
+registerRoute(new NavigationRoute(
+  createHandlerBoundToURL('index.html'),
+  { allowlist },
+))
+
+self.skipWaiting()
+clientsClaim()
